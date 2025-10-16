b4d01f390ca8af964fd02f2cb84f5337d5951a29
Ddennys <54415294+DennysRivera@users.noreply.github.com>
2025-10-16 10:07:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const functions = require('@google-cloud/functions-framework');
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-functions.http('helloHttp', (req, res) => {
-  res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+exports.helloWorldFB = onRequest({
+  preserveExternalChanges: true,
+  concurrency: 5,
+  timeoutSeconds: 300,
+  memory: "1GiB",
+  region: "us-central1",
+  minInstances: 0,
+  maxInstances: 10,
+  serviceAccount: "crjob-vpcsc@dennysp-dev.iam.gserviceaccount.com",
+}, (request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!!");
 });
